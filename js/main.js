54d419d9618e9fc948675ccfb4d4c71df67fced9
Guy Roberts <guyroberts21@outlook.com>
2020-04-26 11:58:08 +0100

    Local storage working

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
+const storage = window.localStorage
+
 const library = document.getElementById('library');
 const addBookBtn = document.getElementById('submit-book');
 const errorMessage = document.getElementById('error-message');
-let myLibrary = [];
 
-// default books
-let book = new Book('Lord of the Rings', 'J.R.R. Tolkien', 456);
-addBookToLibrary(book);
+let myLibrary = storage.getItem('items') ? JSON.parse(storage.getItem('items')) : [];
+storage.setItem('items', JSON.stringify(myLibrary));
 
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     
-    this.info = function() {
-        return `${this.title} | ${this.author} | ${this.pages} pages`;
+    // this.info = function() {
+    //     return `${this.title} | ${this.author} | ${this.pages} pages`;
+    // }
+}
+
+function submitBook() {
+    let title = document.getElementById('btitle').value;
+    let author = document.getElementById('bauthor').value;
+    let pages = document.getElementById('bpages').value;
+    
+    let book = new Book(title, author, pages);
+    
+    // render new book on page
+    if (title && author && pages) {
+        myLibrary.push(book);
+        storage.setItem('items', JSON.stringify(myLibrary));
+
+        errorMessage.textContent = "";
+        modal.style.display = 'none';
+        library.innerHTML = "";
+        render();
+    } else {
+        errorMessage.textContent = "Please fill out all the fields";
     }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
 function removeBook(e) {
-    let container = e.currentTarget.parentNode;
     let idx = e.currentTarget.parentNode.dataset.book;
     myLibrary.splice(idx, 1);
     library.innerHTML = "";
     render();
 }
 
+
 function render() {
     for (let book of myLibrary) {
         // book container
         const bookContainer = document.createElement('li');
-        bookContainer.textContent = book.info();
+        bookContainer.textContent = `${book.title} | ${book.author} | ${book.pages} pages`
         bookContainer.dataset.book = `${myLibrary.length - 1}`;
         
         // delete btn
@@ -50,24 +67,8 @@ function render() {
     }
 }
 
-function submitBook() {
-    let title = document.getElementById('btitle').value;
-    let author = document.getElementById('bauthor').value;
-    let pages = document.getElementById('bpages').value;
-    
-    let book = new Book(title, author, pages);
-    
-    // render new book on page
-    if (title && author && pages) {
-        errorMessage.textContent = "";
-        myLibrary.push(book);
-        modal.style.display = 'none';
-        library.innerHTML = "";
-        render();
-    } else {
-        errorMessage.textContent = "Please fill out all the fields";
-    }
+if (myLibrary.length) {
+    render();
 }
 
-render();
 addBookBtn.addEventListener('click', submitBook);
